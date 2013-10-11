c4df100ecd02f3c0ccee665c0461d21c6d16603f
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-11 18:31:09 -0500

    seeded database and deck

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -2,4 +2,5 @@ class Guess < ActiveRecord::Base
   # Remember to create a migration!
 
   belongs_to :game
+  has_one :user, through: :game
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,14 @@
-require 'faker'
 
+User.create(user_name: 'Elaine', password: "dbc")
 
-5.times do
-  Deck.create(title: Faker::Company.catch_phrase)
-  Card.create(question: "#{Faker::Company.bs}?", expected_answer: Faker::Company.name )
-  User.create(user_name: Faker::Internet.user_name, password: Faker::Base.bothify("password"))
+deck = Deck.create(title: "U.S. Capitals", photo_url: "http://upload.wikimedia.org/wikipedia/commons/2/27/Capitol_Building_Full_View.jpg")
+
+cards = [Card.create(question: "What is the capital of Illinois?", expected_answer: "Springfield"),
+        Card.create(question: "What is the capital of Missouri?", expected_answer: "Jefferson City"),
+        Card.create(question: "What is the capital of California?", expected_answer: "Sacramento"),
+        Card.create(question: "What is the capital of New York?", expected_answer: "Albany"),
+        Card.create(question: "What is the capital of Vermont?", expected_answer: "Montpelier")]
+
+cards.each do |card|
+  deck.cards << card
 end
