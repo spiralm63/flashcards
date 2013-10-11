0b650bc4b380b435a5a347fb74769500b4ab5eb4
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-11 18:38:30 -0500

    deck layout and other stuff

--- public/js/application.js
+++ public/js/application.js
@@ -1,2 +1,9 @@
 $(document).ready(function(){
+  $(".button").click(function(event){
+    event.preventDefault();
+    $(".deck-box").flippy({
+    duration: "300",
+    verso: '<img id="deck-title-image" src="http://cdn2.sbnation.com/uploads/chorus_image/image/14219359/jpeg.0_cinema_640.0.jpeg" />'),
+    });
+  });
 });
